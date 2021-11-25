6192236471a7e4105f5769b838e5f5a556bec757
Matheus Schneider Camilo <matheus.schneider.camilo@gmail.com>
2021-11-25 01:08:13 -0300

    Add permission to microphone and camera

--- src/screens/Meeting/index.tsx
+++ src/screens/Meeting/index.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { requestPermissions } from '../../services/permissions';
 
 const Meeting = () => {
     const [uri, setURI] = useState('');
-    const userAgent =
-        'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36';
+
     Linking.getInitialURL().then((value) => {
         if (!value) {
             return;
         }
+
+        requestPermissions(['camera', 'microphone']);
 
         value = value.replace('view.php?', 'bbb_view.php?action=join&');
 
@@ -18,7 +20,7 @@ const Meeting = () => {
 
     return (
         <WebView
-            userAgent={userAgent}
+            userAgent={''}
             source={{
                 uri,
             }}
---
+++ src/services/permissions/index.ts
@@ -0,0 +1,33 @@
+import { Platform } from 'react-native';
+import {
+    Permission,
+    PERMISSIONS,
+    requestMultiple,
+} from 'react-native-permissions';
+
+export const requestPermissions = async (
+    request: Array<'camera' | 'microphone'>,
+) => {
+    const permissions: Permission[] = [];
+
+    if (request.includes('camera')) {
+        permissions.push(
+            Platform.OS === 'ios'
+                ? PERMISSIONS.IOS.CAMERA
+                : PERMISSIONS.ANDROID.CAMERA,
+        );
+    }
+    if (request.includes('microphone')) {
+        permissions.push(
+            Platform.OS === 'ios'
+                ? PERMISSIONS.IOS.MICROPHONE
+                : PERMISSIONS.ANDROID.RECORD_AUDIO,
+        );
+    }
+
+    const result = await requestMultiple(permissions);
+
+    if (Object.values(result).some((status) => status !== 'granted')) {
+        throw new Error('User did not grant all permissions.');
+    }
+};
