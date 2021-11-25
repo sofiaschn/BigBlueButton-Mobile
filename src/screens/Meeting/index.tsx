4c0329813730271562e53e23b6e7145011765699
Matheus Schneider <matheus.schneider@khomp.com>
2021-11-24 21:12:31 -0300

    Add WebView to show the BBB page

--- src/screens/Meeting/index.tsx
+++ src/screens/Meeting/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-const Home = () => {
-    return <View />;
+const Meeting = () => {
+    const userAgent =
+        'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36';
+    const uri =
+        'https://live-aws001.mconf.rnp.br/html5client/join';
+
+    return (
+        <WebView
+            userAgent={userAgent}
+            source={{
+                uri,
+            }}
+        />
+    );
 };
 
-export default Home;
+export default Meeting;
